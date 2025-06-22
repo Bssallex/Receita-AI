a5d5e421aa140286651151aec05999a9b8607440
Alex Bruno <Bssallex@gmail.com>
2025-06-21 21:46:28 -0300

    first commit

---
+++ src/main/java/ReceitaAI/ReceitaAiApplication.java
@@ -0,0 +1,13 @@
+package ReceitaAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceitaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceitaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ReceitaAI/ReceitaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package ReceitaAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
